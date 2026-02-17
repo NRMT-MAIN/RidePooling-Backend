4d0a04ecd9265b0e69a68509341a1948d9146b29
NRMT-MAIN <sahunirmit.main@gmail.com>
2026-02-17 19:27:03 +0530

    Implemented Matching Service Business Logic

--- src/main/java/com/nirmit/ride_pooling/repository/CabRepository.java
+++ src/main/java/com/nirmit/ride_pooling/repository/CabRepository.java
@@ -1,9 +1,13 @@
 package com.nirmit.ride_pooling.repository;
 
 import com.nirmit.ride_pooling.entity.Cab;
+import com.nirmit.ride_pooling.entity.CabStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CabRepository extends JpaRepository<Cab , Long> {
+    List<Cab> findByStatus(CabStatus status) ;
 }
---
+++ src/main/java/com/nirmit/ride_pooling/service/MatchingService.java
@@ -0,0 +1,77 @@
+package com.nirmit.ride_pooling.service;
+
+import com.nirmit.ride_pooling.entity.*;
+import com.nirmit.ride_pooling.repository.CabRepository;
+import com.nirmit.ride_pooling.repository.RideRepository;
+import com.nirmit.ride_pooling.repository.RideRequestRepository;
+import com.nirmit.ride_pooling.validators.ConstraintValidator;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class MatchingService {
+    private final RideRepository rideRepository ;
+    private final RideRequestRepository rideRequestRepository ;
+    private final CabRepository cabRepository ;
+    private final ConstraintValidator constraintValidator ;
+
+    @Transactional
+    public void match(RideRequest newRequest) {
+        String prefix = newRequest.getPickupGeohash().substring(0 , 5) ;
+
+        List<RideRequest> candidates = rideRequestRepository.findAndLockCandidates(prefix , 10) ;
+
+        for(RideRequest candidate : candidates) {
+            if (candidate.getRide() != null) {
+                Ride ride = rideRepository
+                        .findByIdForUpdate(candidate.getRide().getId())
+                        .orElseThrow() ;
+
+                if(constraintValidator.canMerge(ride , candidate , newRequest)) {
+                    attachToRide(ride , newRequest) ;
+                    return;
+                }
+            }
+        }
+
+        createNewRide(newRequest) ;
+    }
+
+    private void createNewRide(RideRequest request) {
+        Cab availableCab = cabRepository
+                .findByStatus(CabStatus.AVAILABLE)
+                .stream()
+                .findFirst()
+                .orElseThrow(() -> new RuntimeException("No cab available")) ;
+
+        Ride ride = Ride.builder()
+                .cab(availableCab)
+                .status(RideStatus.FORMING)
+                .totalSeatsUsed(request.getSeatsRequired())
+                .totalLuggageUsed(request.getLuggageCount())
+                .estimatedTotalTime(0)
+                .build();
+
+        rideRepository.save(ride) ;
+        request.setRide(ride);
+        request.setStatus(RideRequestStatus.WAITING);
+
+        rideRequestRepository.save(request) ;
+    }
+
+    private void attachToRide(Ride ride , RideRequest request) {
+        ride.setTotalSeatsUsed(ride.getTotalSeatsUsed() + request.getSeatsRequired());
+
+        ride.setTotalLuggageUsed(ride.getTotalSeatsUsed() + request.getLuggageCount());
+
+        request.setRide(ride);
+        request.setStatus(RideRequestStatus.MATCHED);
+
+        rideRepository.save(ride) ;
+        rideRequestRepository.save(request) ;
+    }
+}
