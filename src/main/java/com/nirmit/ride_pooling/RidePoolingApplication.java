71ece86ba40a6db57e5c719180661db0d7f4b816
NRMT-MAIN <sahunirmit.main@gmail.com>
2026-02-17 11:35:54 +0530

    Initial Commit

---
+++ src/main/java/com/nirmit/ride_pooling/RidePoolingApplication.java
@@ -0,0 +1,13 @@
+package com.nirmit.ride_pooling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RidePoolingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RidePoolingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nirmit/ride_pooling/RidePoolingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nirmit.ride_pooling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RidePoolingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
